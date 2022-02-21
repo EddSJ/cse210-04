2fa7122b51bc4666c4c8fb9fa00aff30791743cc
shauniebyrne <byr21002@byui.edu>
2022-02-21 10:51:20 -0700

    additions

--- greed/greed/game/casting/gems.py
+++ greed/greed/game/casting/gems.py
@@ -3,13 +3,26 @@ from game.casting.actor import Actor
 
 class Gem(Actor):
     """
-    An item of cultural or historical interest. 
+    A gem (*) that the user will try to catch. 
     
-    The responsibility of an Artifact is to provide a message about itself.
+    The responsibility of a Gem is to provide something for the user to earn
+    points catching.
 
     Attributes:
-        _message (string): A short description about the artifact.
+        _points (100): points earned by getting gems.
     """
     def __init__(self):
         super().__init__()
-        self._points = 0
+        self._points = 100
+
+    def add_points(self):
+        """
+        Add 100 points everytime a gem (*) is caught by the user.
+
+        Arg:
+            (self): An instance of the Gem class.
+
+        Return:
+            points: added points
+        """
+        return self._points + 100
--- greed/greed/game/casting/rocks.py
+++ greed/greed/game/casting/rocks.py
@@ -3,14 +3,26 @@ from game.casting.actor import Actor
 
 class Rock(Actor):
     """
-    An item of cultural or historical interest. 
+    A rock (o) that the user will try to avoid. 
     
-    The responsibility of an Artifact is to provide a message about itself.
+    The responsibility of a Rock is to provide something for the user to try
+    to avoid and to loose points if caught.
 
     Attributes:
-        _message (string): A short description about the artifact.
+        _points (0): points lost by accidentally catching rocks.
     """
     def __init__(self):
         super().__init__()
         self._points = 0
         
+    def subract_points(self):
+        """
+        Subract 100 points everytime a rock (o) is caught by the user.
+
+        Arg:
+            (self): An instance of the Rock class.
+
+        Return:
+            points: subracted points
+        """
+        return self._points - 100
